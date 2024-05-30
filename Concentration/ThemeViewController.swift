2986901c1253934bd67a169b9c070b302718a159
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2024-05-30 12:20:39 +0600

    made split view type as unspecified, note you have to drag from left in ipad to see master page now

--- Concentration/ThemeViewController.swift
+++ Concentration/ThemeViewController.swift
@@ -7,10 +7,9 @@
 
 import UIKit
 
-class ThemeViewController: UIViewController {
-//  class ThemeViewController: UIViewController, UISplitViewControllerDelegate {
+class ThemeViewController: UIViewController, UISplitViewControllerDelegate {
     // making this strong will retain previous game but makes game unresonsive, to fix it SplitViewController delegate must be set (for iphone)
-    // private var lastGame: ConcentrationViewController?
+    private var lastGame: ConcentrationViewController?
     private var gameNavigationController: UINavigationController? {
         return splitViewController?.viewControllers.last as? UINavigationController
     }
@@ -19,22 +18,21 @@ class ThemeViewController: UIViewController {
     }
     
     // this do not work if SplitViewController is set double column (by default), fixed by setting it unspecified from storyboard
-    // but making this unspecified from storyboard makes master layout invisible at ipad, so looking for a solution
-    //    override func awakeFromNib() {
-    //        super.awakeFromNib()
-    //        splitViewController?.delegate = self
-    //    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        splitViewController?.delegate = self
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    //    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
-    //        if lastGame != nil {
-    //            return false
-    //        }
-    //        return true
-    //    }
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
+        if lastGame != nil {
+            return false
+        }
+        return true
+    }
 
     @IBAction func changeTheme(_ sender: UIButton) {
         if let theme = sender.currentTitle {
@@ -42,16 +40,13 @@ class ThemeViewController: UIViewController {
             if let game = gameViewController {
                 // for ipad
                 game.resetTheme(getTheme(theme))
+            } else if lastGame != nil {
+                // for iphone
+                lastGame?.resetTheme(getTheme(theme))
+                gameNavigationController?.pushViewController(lastGame!, animated: true)
             } else {
                 performSegue(withIdentifier: "Show Game", sender: sender)
             }
-            
-            // looking for a solution, as the only way to make master of SplitViewController be shown on iphone is to make its style unspecified
-            //            else if lastGame != nil {
-            //                // for iphone
-            //                lastGame?.resetTheme(getTheme(theme))
-            //                gameNavigationController?.pushViewController(lastGame!, animated: true)
-            //            }
         }
     }
     
@@ -69,7 +64,7 @@ class ThemeViewController: UIViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "Show Game", let game = segue.destination as? ConcentrationViewController, let theme = (sender as? UIButton)?.currentTitle {
-            //            lastGame = game
+            lastGame = game
             game.theme = getTheme(theme)
         }
     }
